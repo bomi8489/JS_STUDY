7ba25fba0e800a182df8596bbd7b7bee21092ffe
AUTUMN <krshlee22@naver.com>
2021-08-06 10:05:56 +0900

    퇴근하고싶다

--- 3일차/todo.js
+++ 3일차/todo.js
@@ -8,9 +8,11 @@ function show_list(text) {
   const div = document.createElement("div");
   div.innerText = text;
   delbtn.innerText = "지우기";
+
   delbtn.addEventListener("click", (e) => { //클릭시 li태그를 삭제하는 이벤트리스너
     list.removeChild(e.target.parentElement);
   });
+  
   li.appendChild(div);      // li태그에 div 자식추가
   li.appendChild(delbtn);   // li태그에 belbtn 자식추가
   list.appendChild(li);     // list(ul태그)에 li태그 추가
